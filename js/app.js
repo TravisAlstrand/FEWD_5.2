e61a533db608724c51a49f8912c07717ae6fbd50
Travis Alstrand <travisalstrand@gmail.com>
2022-11-15 17:02:23 -0800

    completed

--- js/app.js
+++ js/app.js
@@ -4,3 +4,23 @@ baguetteBox.run('.gallery', {
   animation: 'fadeIn',
   overlayBackgroundColor: 'rgba(0,0,0,0.7)'
 });
+
+
+// SEARCH FUNCTIONALITY
+
+const searchBar = document.querySelector('input');
+const images = document.querySelectorAll('a');
+
+searchBar.addEventListener('keyup', e => {
+  const searchText = e.target.value.toLowerCase();
+  images.forEach(img => {
+    const caption = img.getAttribute('data-caption');
+    const lowCap = caption.toLowerCase();
+
+    if (!lowCap.includes(searchText)) {
+      img.style.display = 'none';
+    } else {
+      img.style.display = 'inherit';
+    }
+  })
+})
